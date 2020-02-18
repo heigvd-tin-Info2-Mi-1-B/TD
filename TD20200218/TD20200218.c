4c4d42619da7b5c34b9b9997361336bf1f80755c
Pierre BRESSY <pierre.bressy@heig-vd.ch>
2020-02-18 14:12:56 +0100

    New files for TD20200218

---
+++ TD20200218/TD20200218.c
@@ -0,0 +1,17 @@
+/**
+ \file	TD20200218.c
+ \brief	 
+ \author	Pierre BRESSY
+ \version	1.0
+ \date	11.10.2019, 10:16:39
+ \details
+ **/
+
+#include "TD20200218.h"
+
+
+int main(int argc, char const *argv[])
+{
+    puts("Hi !");
+    return 0;
+}
---
+++ TD20200218/TD20200218.h
@@ -0,0 +1,27 @@
+/**
+ \file	TD20200218.h
+ \brief	header file for TD20200218.c
+ \author	Pierre BRESSY
+ \version	1.0
+ \date	11.10.2019, 10:18:09
+ \details
+    
+ **/
+
+#pragma once
+#include <stdint.h>
+#include <stdio.h>
+
+// preprocessor symbols
+
+// preprocessor macros
+
+// enumerated types
+
+// structured types
+
+// unions types
+
+// functions prototypes
+
+// externals
