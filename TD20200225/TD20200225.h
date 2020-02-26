6dc963ca2520b1a065c1a57fbacc0e8591d4b912
Pierre BRESSY <pierre.bressy@heig-vd.ch>
2020-02-26 09:36:37 +0100

    added TD20200225
    text file basics

---
+++ TD20200225/TD20200225.h
@@ -0,0 +1,25 @@
+/**
+ * \file TD20200225.h
+ * \version 1.0
+ * \date 25/02/2020 - 15:33:28
+ * \brief header to TD20200225
+ */
+
+#pragma once
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+// preprocessor symbols
+
+// preprocessor macros
+
+// enumerated types
+
+// structured types
+
+// unions types
+
+// functions prototypes
+
+// externals
