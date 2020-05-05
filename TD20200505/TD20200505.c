e13d4c91fc76303ea5ee172143783294f9a39f76
Pierre BRESSY <pierre.bressy@heig-vd.ch>
2020-05-05 18:01:37 +0200

    added TD20200505

---
+++ TD20200505/TD20200505.c
@@ -0,0 +1,25 @@
+/**
+  \file      TD20200505.c
+  \brief     multiple file compilation
+  \author    Pierre BRESSY
+  \version   1.0
+  \date      2020-04-21 13:41:24
+  \details
+    
+**/
+
+#include "TD20200505.h"
+
+int main(int argc, char const *argv[])
+{
+  //displayError(E_ALLOC_MEM_ERROR);
+
+  sPoint p1 = {1., 1.};
+  sPoint p2 = {0., 0.};
+
+  p2 = rotate(p1, -45);
+
+  pointDisplay("P1", p1);
+  pointDisplay("P2", p2);
+  return 0;
+}
